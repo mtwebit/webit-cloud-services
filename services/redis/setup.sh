ad85bf62d217930e5800ecef1f886a88c19aab2d
mtwebit <mt+git@webit.hu>
2019-07-24 13:23:04 +0200

    Basic Redis support

---
+++ services/redis/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Webit Cloud Services Toolkit - Onlyoffice
+#
+# Copyright 2019 Tamas Meszaros <mt+git@webit.hu>
+# Licensed under Mozilla Public License v2.0 http://mozilla.org/MPL/2.0/
+# 
+
+title="Redis"
+desc="A key-value data store"
+dockerimage="redis"
+
+if ! container_exists $containername; then
+  siport="6379"
+  service_setup
+  container_setup "$dockerimage" $containername
+else
+  if askif "Update the $title service?" y; then
+    container_remove $containername
+    . ${BASH_SOURCE[0]}
+  fi
+fi
+
+# Start the container if it is not running
+container_running $containername || container_start $containername
+
+return 0
