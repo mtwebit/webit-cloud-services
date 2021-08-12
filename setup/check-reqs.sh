327e2eb201ff0c256d986cbd9e03608ba3fca4c8
mtwebit <mtwebit@users.noreply.github.com>
2021-08-12 18:33:25 +0200

    Update check-reqs.sh
    Check if curl and whiptail exists.

--- setup/check-reqs.sh
+++ setup/check-reqs.sh
@@ -7,6 +7,8 @@
 # 
 
 which docker 2>/dev/null >/dev/null || docker_install
+which curl 2>/dev/null >/dev/null || fatal "Install curl and try again."
+which whiptail 2>/dev/null >/dev/null || fatal "Install whiptail (newt) and try again."
 [ `id -u` != 0 ] && [ "`groups | grep '\bdocker\b'`" == "" ] && fatal "You are not member of the Docker group."
 docker ps 2>/dev/null >/dev/null || fatal "Docker is not running."
 
